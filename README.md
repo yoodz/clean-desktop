9caa3ff817172b8b005779ec1f8e669a7b5c1e8a
zhengdayong <zhengdayong@gg.com>
2019-07-25 14:01:07 +0800

    init

---
+++ README.md
@@ -0,0 +1,9 @@
+## clean your desktop on window
+A command to organize your messy window desktop. Put files in different folders according to file suffix.
+一个命令整理你杂乱的window桌面。根据文件后缀放在指定的文件夹
+Usage
+```
+npm install clean-desktop -g
+```
+then open your cmd run clean-desktop
+然后打开命令行输入 clean-desktop
---
+++ index.js
@@ -0,0 +1,79 @@
+#!/usr/bin/env node
+
+var path = require("path");
+var fs = require("fs");
+var readlineSync = require('readline-sync');
+var os = require('os');
+let desktopPath = path.join(os.homedir(), 'Desktop')
+
+let fileName = readlineSync.question('Please enter the file name after finishing(Archive): ');
+if(!fileName){
+  fileName = 'Archive'
+}
+let afterSort = desktopPath + path.sep + fileName
+
+async function sort() {
+  let exist = await getStat(afterSort)
+  if (!exist) {
+    await mkdir(afterSort)
+  }
+  let dirs = fs.readdirSync(desktopPath)
+
+  dirs.forEach((item) => {
+    let created = fs.readdirSync(afterSort)
+    let fileType = fs.statSync(desktopPath + path.sep + item)
+    if (fileType.isFile()) {
+      if (item.indexOf('.') !== -1 && item.indexOf('.lnk') === -1) {
+        let end = item.split('.')[1]
+        let endPath = afterSort + path.sep + end
+        if (!created.includes(end)) {
+          fs.mkdirSync(endPath);
+        }
+        copyFile(desktopPath + path.sep + item, endPath + path.sep + item)
+      }
+    }
+  })
+}
+
+// 复制文件
+function copyFile(src, dist) {
+  let rs = fs.createReadStream(src)
+  rs.pipe(fs.createWriteStream(dist));
+  rs.on('end', function () {
+    //删除文件
+    fs.unlink(src, function () {
+      console.log('done');
+    })
+  })
+}
+
+function getStat(path) {
+  return new Promise((resolve, reject) => {
+    fs.stat(path, (err, stats) => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(stats);
+      }
+    })
+  })
+}
+
+/**
+ * 创建路径
+ * @param {string} dir 路径
+ */
+function mkdir(dir) {
+  return new Promise((resolve, reject) => {
+    fs.mkdir(dir, err => {
+      if (err) {
+        resolve(false);
+      } else {
+        resolve(true);
+      }
+    })
+  })
+}
+
+sort()
+console.log('Finished')
